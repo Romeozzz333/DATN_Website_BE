7113e13432548d48a3140a2b9ac5aff1ad3e8b0f
HoangTran <HoangTranHoangkhongmacao@gmail.com>
2025-03-13 10:19:02 +0700

    eighth commit

--- src/main/java/org/example/datn_website_be/service/ProductService.java
+++ src/main/java/org/example/datn_website_be/service/ProductService.java
@@ -3,13 +3,11 @@ package org.example.datn_website_be.service;
 import org.example.datn_website_be.dto.request.CartRequest;
 import org.example.datn_website_be.dto.request.ProductDetailPromoRequest;
 import org.example.datn_website_be.dto.request.UpdateProduct.UpdateProductProductUnitsRequest;
-import org.example.datn_website_be.model.ProductUnits;
+import org.example.datn_website_be.model.*;
 import org.springframework.transaction.annotation.Transactional;
 import org.example.datn_website_be.Enum.Status;
 import org.example.datn_website_be.dto.request.ProductRequest;
 import org.example.datn_website_be.dto.response.*;
-import org.example.datn_website_be.model.Category;
-import org.example.datn_website_be.model.Product;
 import org.example.datn_website_be.repository.*;
 import org.example.datn_website_be.webconfig.NotificationController;
 import org.springframework.beans.BeanUtils;
@@ -49,6 +47,12 @@ public class ProductService {
     ProductUnitsRepository productUnitsRepository;
 
     @Autowired
+    public ProductHistoryRepository productHistoryRepository;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
     NotificationController notificationController;
 
     @Transactional
@@ -78,6 +82,13 @@ public class ProductService {
         if (!checkProductDetail) {
             throw new RuntimeException("Xảy ra lỗi khi thêm sản phẩm chi tiết");
         }
+        Account account = accountService.getUseLogin();
+        ProductHistory productHistory =  ProductHistory.builder()
+                .note(account.getEmail()+" đã thêm sản phẩm mới")
+                .account(account)
+                .product(saveProduct)
+                .build();
+        productHistoryRepository.save(productHistory);
     }
 
     @Transactional
@@ -106,13 +117,40 @@ public class ProductService {
             productImageRepository.deleteByProduct(saveProduct);
             boolean checkImage = productImageService.createProductImage(saveProduct, updateProductProductUnitsRequest.getProductRequest().getListImages());
             if (!checkImage) {
-                throw new RuntimeException("Xảy ra lỗi khi thêm ảnh cho sản phẩm chi tiết");
+                throw new RuntimeException("Xảy ra lỗi khi thêm ảnh cho sản phẩm");
             }
         }
         if (!updateProductProductUnitsRequest.getIdProductUnits().isEmpty()) {
             productUnitsService.deleteProductUnits(updateProductProductUnitsRequest.getIdProductUnits());
         }
+        Account account = accountService.getUseLogin();
+        ProductHistory productHistory =  ProductHistory.builder()
+                .note(account.getEmail()+" đã cập nhật sản phẩm")
+                .account(account)
+                .product(saveProduct)
+                .build();
+        productHistoryRepository.save(productHistory);
         notificationController.sendNotification();
+    }
+    public void ExportProduct(Long id, double quantity) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy tài nguyên sản phẩm trong hệ thống!"));
+        if (quantity>product.getQuantity()){
+            throw new RuntimeException("Số lượng hủy vượt quá số lượng sản phẩm");
+        }
+        double newQuantity = product.getQuantity() - quantity;
+        if(newQuantity == 0.0){
+            product.setStatus(Status.INACTIVE.toString());
+        }
+        product.setQuantity(newQuantity);
+        productRepository.save(product);
+        Account account = accountService.getUseLogin();
+        ProductHistory productHistory =  ProductHistory.builder()
+                .note(account.getEmail()+" đã xuất hủy sản phẩm")
+                .account(account)
+                .product(product)
+                .build();
+        productHistoryRepository.save(productHistory);
     }
 
     public ProductResponse findProductById(Long id) {
@@ -154,6 +192,9 @@ public class ProductService {
         if (!optionalProduct.isPresent()) {
             throw new RuntimeException("Không tìm thấy tài nguyên sản phẩm trong hệ thống!");
         }
+        if(optionalProduct.get().getQuantity()<=0.0){
+            throw new RuntimeException("Vui lòng cập nhật lại số lượng của sản phẩm trước khi thay đổi trạng thái!");
+        }
         String newStatus = aBoolean ? Status.ACTIVE.toString() : Status.INACTIVE.toString();
         optionalProduct.get().setStatus(newStatus);
         Product product = productRepository.save(optionalProduct.get());
@@ -162,6 +203,13 @@ public class ProductService {
             units.setStatus(newStatus);
             productUnitsRepository.save(units);
         }
+        Account account = accountService.getUseLogin();
+        ProductHistory productHistory =  ProductHistory.builder()
+                .note(account.getEmail()+" đã thay đổi trạng thái sản phẩm "+newStatus)
+                .account(account)
+                .product(product)
+                .build();
+        productHistoryRepository.save(productHistory);
         notificationController.sendNotification();
         return product;
     }
